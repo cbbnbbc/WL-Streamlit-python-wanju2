943381c599551814e8e210dd4366d860d8338b96
cbbnbbc <59731650+cbbnbbc@users.noreply.github.com>
2025-03-16 14:27:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,5 +93,5 @@ if os.path.exists(video_path):
 # Define the URL of the website you want to proxy
 url = "https://douyin.boo/index.html"
 # 去掉下面一句前面#，可以显示网页版抖音美女
- st.components.v1.html(f'<iframe src="{url}" width="100%" height="600" style="border:none;"></iframe>', height=700)
+# st.components.v1.html(f'<iframe src="{url}" width="100%" height="600" style="border:none;"></iframe>', height=700)
 
