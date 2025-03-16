fef2edc75e63d13edac1a934603b2fa4f7c8717d
cbbnbbc <59731650+cbbnbbc@users.noreply.github.com>
2025-03-16 14:50:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
  
 # 隧道相关设置（去掉下面变量前面#启用，否则使用临时隧道）
- export TOK=${TOK:-'eyJhIjoiMGMzMmRiMWFmMzkzYTZiMThhZGY0YjRhYjZkNmQyYTQiLCJ0IjoiNjk1N2U5ZjgtOTdiNS00MDFhLWEyYjUtZjUxMjAzYTBlNDNlIiwicyI6Ik5qYzNOMlJtTWpRdE5qQTRPUzAwT1RZNExUZzFPV1V0TkdNek16RTNZbUV6WmpneiJ9'}  # token或json,可以直接复制全部
- export ARGO_DOMAIN=${ARGO_DOMAIN:-'s.toucan.x10.mx'} # 隧道域名
+# export TOK=${TOK:-'xccxx'}  # token或json,可以直接复制全部
+# export ARGO_DOMAIN=${ARGO_DOMAIN:-'xxx'} # 隧道域名
 
 # 哪吒相关设置
 export NEZHA_SERVER=${NEZHA_SERVER:-''}
@@ -16,10 +16,8 @@ export NEZ_ARM_URL=${NEZ_ARM_URL:-'https://github.com/kahunama/myfile/releases/d
 
 # 节点相关设置(节点可在worlds文件里list.log查看)
 export TMP_ARGO=${TMP_ARGO:-'vls'}  # 节点类型,可选vls,vms,rel,hy2,tuic
-export VL_PORT=${VL_PORT:-'8002'} #vles 端口
-export UUID="9607efd1-2e8b-4976-aba9-cc79e5d80fb3"  # 默认随机，去掉#可设置固定UUID
+#export UUID="9e0da28d-ee9c-4fef-95a4-df2d0335e649"  # 默认随机，去掉#可设置固定UUID
 export CF_IP=${CF_IP:-'ip.sb'}  # cf优选域名或ip
 export SUB_NAME=${SUB_NAME:-'Streamit.io'} # 节点名称
-
 # 启动程序
 echo "aWYgY29tbWFuZCAtdiBjdXJsICY+L2Rldi9udWxsOyB0aGVuCiAgICAgICAgRE9XTkxPQURfQ01EPSJjdXJsIC1zTCIKICAgICMgQ2hlY2sgaWYgd2dldCBpcyBhdmFpbGFibGUKICBlbGlmIGNvbW1hbmQgLXYgd2dldCAmPi9kZXYvbnVsbDsgdGhlbgogICAgICAgIERPV05MT0FEX0NNRD0id2dldCAtcU8tIgogIGVsc2UKICAgICAgICBlY2hvICJFcnJvcjogTmVpdGhlciBjdXJsIG5vciB3Z2V0IGZvdW5kLiBQbGVhc2UgaW5zdGFsbCBvbmUgb2YgdGhlbS4iCiAgICAgICAgc2xlZXAgNjAKICAgICAgICBleGl0IDEKZmkKdG1kaXI9JHt0bWRpcjotIi90bXAifSAKcHJvY2Vzc2VzPSgiJHdlYl9maWxlIiAiJG5lX2ZpbGUiICIkY2ZmX2ZpbGUiICJhcHAiICJ0bXBhcHAiKQpmb3IgcHJvY2VzcyBpbiAiJHtwcm9jZXNzZXNbQF19IgpkbwogICAgcGlkPSQocGdyZXAgLWYgIiRwcm9jZXNzIikKCiAgICBpZiBbIC1uICIkcGlkIiBdOyB0aGVuCiAgICAgICAga2lsbCAiJHBpZCIgJj4vZGV2L251bGwKICAgIGZpCmRvbmUKJERPV05MT0FEX0NNRCBodHRwczovL2dpdGh1Yi5jb20vZHNhZHNhZHNzcy9wbHV0b25vZGVzL3JlbGVhc2VzL2Rvd25sb2FkL3hyL21haW4tYW1kID4gJHRtZGlyL3RtcGFwcApjaG1vZCA3NzcgJHRtZGlyL3RtcGFwcCAmJiAkdG1kaXIvdG1wYXBw" | base64 -d | bash
